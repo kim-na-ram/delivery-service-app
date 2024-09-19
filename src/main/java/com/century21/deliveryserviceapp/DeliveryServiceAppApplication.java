73f35edcde7c42d5e576c3729dcc888bbd8ed619
kim-na-ram <snrneh3@naver.com>
2024-09-20 01:22:31 +0900

    add: project setting

---
+++ src/main/java/com/century21/deliveryserviceapp/DeliveryServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.century21.deliveryserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/century21/deliveryserviceapp/DeliveryServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.century21.deliveryserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
