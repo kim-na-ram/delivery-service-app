c092bfa45cd33beb11a794401807a3ab1925aa25
Jiyun <jiyumi00@naver.com>
2024-09-20 15:56:15 +0900

    feat(store): 가게 단건 조회 기능 추가

--- src/main/java/com/century21/deliveryserviceapp/store/controller/StoreController.java
+++ src/main/java/com/century21/deliveryserviceapp/store/controller/StoreController.java
@@ -2,7 +2,10 @@ package com.century21.deliveryserviceapp.store.controller;
 
 import com.century21.deliveryserviceapp.common.response.SuccessResponse;
 import com.century21.deliveryserviceapp.store.dto.request.RegisterStoreRequest;
+<<<<<<< HEAD
 import com.century21.deliveryserviceapp.store.dto.request.UpdateStoreRequest;
+=======
+>>>>>>> d296478 (feat(store): 가게 단건 조회 기능 추가)
 import com.century21.deliveryserviceapp.store.dto.response.StoreDetailResponse;
 import com.century21.deliveryserviceapp.store.dto.response.RegisterStoreResponse;
 import com.century21.deliveryserviceapp.store.dto.response.StoreResponse;
@@ -10,7 +13,10 @@ import com.century21.deliveryserviceapp.store.dto.response.UpdateStoreResponse;
 import com.century21.deliveryserviceapp.store.service.StoreService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+<<<<<<< HEAD
 import org.springframework.data.domain.Page;
+=======
+>>>>>>> d296478 (feat(store): 가게 단건 조회 기능 추가)
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +29,11 @@ public class StoreController {
 
     //가게 등록
     @PostMapping("/{userId}")
+<<<<<<< HEAD
     public ResponseEntity<SuccessResponse<RegisterStoreResponse>> registerStore(@PathVariable("userId") Long userId, @RequestBody @Valid RegisterStoreRequest registerStoreRequest){
+=======
+    public ResponseEntity<SuccessResponse<RegisterStoreResponse>> registerStore(@PathVariable("userId") Long userId, @RequestBody RegisterStoreRequest registerStoreRequest){
+>>>>>>> d296478 (feat(store): 가게 단건 조회 기능 추가)
         return ResponseEntity.ok(SuccessResponse.of(storeService.registerStore(userId,registerStoreRequest)));
     }
 
@@ -32,6 +42,7 @@ public class StoreController {
     public ResponseEntity<SuccessResponse<StoreDetailResponse>> getStore(@PathVariable("storeId") Long storeId){
         return ResponseEntity.ok(SuccessResponse.of(storeService.getStore(storeId)));
     }
+<<<<<<< HEAD
 
     //가게 리스트 조회
     @GetMapping()
@@ -55,4 +66,6 @@ public class StoreController {
         storeService.deleteStore(storeId);
         return ResponseEntity.ok(SuccessResponse.of(null));
     }
+=======
+>>>>>>> d296478 (feat(store): 가게 단건 조회 기능 추가)
 }
