ec04d22ae420935ab9b4485f77383525820f5043
Jiyun <jiyumi00@naver.com>
2024-09-20 17:21:36 +0900

    feat(store): 가게 수정 조회 기능 추가

---
+++ src/main/java/com/century21/deliveryserviceapp/store/dto/response/UpdateStoreResponse.java
@@ -0,0 +1,17 @@
+package com.century21.deliveryserviceapp.store.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Getter
+@AllArgsConstructor
+public class UpdateStoreResponse {
+    private Long storeId;
+    private String storeName;
+    private String introduction;
+    private LocalTime openingTime;
+    private LocalTime closedTime;
+    private int minOrderPrice;
+}
--- src/main/java/com/century21/deliveryserviceapp/store/service/StoreService.java
+++ src/main/java/com/century21/deliveryserviceapp/store/service/StoreService.java
@@ -5,12 +5,16 @@ import com.century21.deliveryserviceapp.common.exception.NotFoundException;
 import com.century21.deliveryserviceapp.entity.Store;
 import com.century21.deliveryserviceapp.entity.User;
 import com.century21.deliveryserviceapp.store.dto.request.RegisterStoreRequest;
+import com.century21.deliveryserviceapp.store.dto.request.UpdateStoreRequest;
 import com.century21.deliveryserviceapp.store.dto.response.StoreDetailResponse;
 import com.century21.deliveryserviceapp.store.dto.response.RegisterStoreResponse;
+import com.century21.deliveryserviceapp.store.dto.response.UpdateStoreResponse;
 import com.century21.deliveryserviceapp.store.repository.StoreRepository;
 import com.century21.deliveryserviceapp.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.century21.deliveryserviceapp.common.exception.ResponseCode.NOT_FOUND_STORE;
 import static com.century21.deliveryserviceapp.common.exception.ResponseCode.NOT_FOUND_USER;
@@ -22,12 +26,12 @@ public class StoreService {
     private final StoreRepository storeRepository;
     private final UserRepository userRepository;
 
+    @Transactional
     public RegisterStoreResponse registerStore(Long userId,RegisterStoreRequest registerStoreRequest) {
         User user=userRepository.findById(userId).orElseThrow(()->
                 new NotFoundException(NOT_FOUND_USER));
 
         Store newStore=Store.from(user,registerStoreRequest);
-
         Store savedStore=storeRepository.save(newStore);
 
         return new RegisterStoreResponse(
@@ -52,5 +56,24 @@ public class StoreService {
                 store.getClosedTime(),
                 store.getMinOrderPrice()
         );
+    }
+
+
+    @Transactional
+    public UpdateStoreResponse updateStore(Long storeId, UpdateStoreRequest updateStoreRequest) {
+        Store store=storeRepository.findById(storeId).orElseThrow(()->
+                new NotFoundException(NOT_FOUND_STORE));
+
+        store.update(updateStoreRequest);
+
+        return new UpdateStoreResponse(
+                store.getId(),
+                store.getName(),
+                store.getIntroduction(),
+                store.getOpeningTime(),
+                store.getClosedTime(),
+                store.getMinOrderPrice()
+        );
+
     }
 }
