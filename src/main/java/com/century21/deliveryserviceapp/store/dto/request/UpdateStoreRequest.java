e2c8b82b024478ff72963ff070eb310072632ef9
Jiyun <jiyumi00@naver.com>
2024-09-23 02:43:01 +0900

    fix(store): OWNER만 가게 등록 가능하도록 수정

--- src/main/java/com/century21/deliveryserviceapp/store/dto/request/UpdateStoreRequest.java
+++ src/main/java/com/century21/deliveryserviceapp/store/dto/request/UpdateStoreRequest.java
@@ -1,5 +1,7 @@
 package com.century21.deliveryserviceapp.store.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalTime;
@@ -7,10 +9,13 @@ import java.time.LocalTime;
 @Getter
 @NoArgsConstructor
 public class UpdateStoreRequest {
+    @NotBlank
     private String storeName;
     private String introduction;
+    @NotNull
     private LocalTime openingTime;
+    @NotNull
     private LocalTime closedTime;
+    @NotNull
     private Integer minOrderPrice;
-
 }
