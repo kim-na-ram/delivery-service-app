ba48a5961503f9b52aa4421a21b38ad2a9ed2f6f
Jiyun <jiyumi00@naver.com>
2024-09-23 17:59:13 +0900

    fix(store): 폐업된 가게 조회 및 수정 불가

--- src/main/java/com/century21/deliveryserviceapp/store/repository/StoreRepository.java
+++ src/main/java/com/century21/deliveryserviceapp/store/repository/StoreRepository.java
@@ -8,11 +8,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
-    Page<Store> findByNameContaining(String name, Pageable pageable);
-
     @Query("SELECT COUNT(*) FROM Store s WHERE s.user.id=:userId AND s.deletedAt IS NULL")
     int findStoreCount(@Param("userId") Long userId);
+
+    Page<Store> findByNameContainingAndDeletedAtIsNull(String name, Pageable pageable);
+
+    Page<Store> findByDeletedAtIsNull(Pageable pageable);
+
+    Optional<Store> findByIdAndDeletedAtIsNull(Long storeId);
 }
