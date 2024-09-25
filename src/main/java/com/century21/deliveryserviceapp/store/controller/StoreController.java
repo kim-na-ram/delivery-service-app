e18d1105d051d03254aea5dd873e280b482df1b9
Jiyun <jiyumi00@naver.com>
2024-09-25 12:27:45 +0900

    fix(store) : Controller 테스트 수정 (리스트 조회 에러)

--- src/main/java/com/century21/deliveryserviceapp/store/controller/StoreController.java
+++ src/main/java/com/century21/deliveryserviceapp/store/controller/StoreController.java
@@ -12,10 +12,12 @@ import com.century21.deliveryserviceapp.store.service.StoreService;
 import com.century21.deliveryserviceapp.user.auth.AuthUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/stores")
--- src/main/java/com/century21/deliveryserviceapp/store/dto/request/RegisterStoreRequest.java
+++ src/main/java/com/century21/deliveryserviceapp/store/dto/request/RegisterStoreRequest.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalTime;
 
 @Getter
